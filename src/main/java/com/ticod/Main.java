28bab6eae2b66db2210ad38feba0d57fbb2ac70c
tico <lours2021@gmail.com>
2023-07-08 19:10:34 +0900

    Update 백준 11720 숫자 합 구하기

---
+++ src/main/java/com/ticod/Main.java
@@ -0,0 +1,11 @@
+package com.ticod;
+
+import com.ticod.baekjoon.Baekjoon11720;
+
+public class Main {
+    public static void main(String[] args) {
+        Baekjoon11720 problem = new Baekjoon11720();
+        int result = problem.solve(problem.input());
+        System.out.println("result : " + result);
+    }
+}
